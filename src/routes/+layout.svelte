ef54c7532b7b0f6607207545d514e7f9d6d20d4c
shanghanrun <idim7@naver.com>
2026-01-28 14:16:27 +0900

    임시 보관파일 .txt로 만듬

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	// import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.svg';
 
 
 	let { children } = $props();
 </script>
 
-<!-- <svelte:head>
+<svelte:head>
 	<link rel="icon" href={favicon} />
-</svelte:head> -->
+</svelte:head>
 
 {@render children()}
